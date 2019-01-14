db0ae670348521faa24379890a2ae60b2e0acc46
junedandhanin <46663604+junedandhanin@users.noreply.github.com>
2019-01-14 11:27:42 +0700

    Update profile.php

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'dTR0SHo0XqlUgjBNlKPyHQm4vJ0BZCGNy3eN4M36W8OsTBmLaQD5vW0yl4Gr0DgfyzZa0zMVr1RoYpPeebROAf3BfuXwwcvKiLhiZ1BMebD2/WR8SmE5kuqxARzxArLAqTzTk+INEvPm3No/Vw0QmAdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U3d8452b9156537665d6b9021ea3c2c05';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
